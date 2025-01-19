0d110557171f126715e2aa8cf803255a76fb8cdb
John Rei Enriquez <johnrei.enriquez@gmail.com>
2025-01-19 15:51:00 +0800

    nuke previous site in master

--- fruit/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from fruit.models import Fruit
-
-
-@admin.register(Fruit)
-class FruitAdmin(admin.ModelAdmin):
-    list_display = ["name", "description"]
-    search_fields = ["name"]
