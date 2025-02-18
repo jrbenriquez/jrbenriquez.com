9e4a90d568d508955af90ec874cbc179976010d1
John Rei Enriquez <johnrei.enriquez@gmail.com>
2025-02-18 23:39:44 +0800

    feature: main blog

---
+++ jrbenriquez/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-q4-2np#)&kx$=1oar7*krm(ez@ljb+@a*1nrxo8z#6u4t@kz1h"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
