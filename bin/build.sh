26fed4ae09087b4ca19cbe4d4c8a87dadb964ad8
John Rei Enriquez <johnrei.enriquez@gmail.com>
2025-03-02 20:30:50 +0800

    fix: removed poetry installation in build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,15 +2,6 @@
 # Exit on error
 set -o errexit
 
-
-# Install poetry
-export POETRY_HOME=/opt/poetry
-python3 -m venv $POETRY_HOME
-$POETRY_HOME/bin/pip install poetry==2.0.0
-$POETRY_HOME/bin/poetry --version
-
-poetry install
-
 # Convert static asset files
 poetry run python manage.py collectstatic --no-input
 
